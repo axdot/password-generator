8cd32662d77dd1f2e8ba370fc22d846edf2dc7bb
Chris <kimballc3@yahoo.com>
2022-03-20 23:45:43 -0500

    almost finished

--- assets/script.js
+++ assets/script.js
@@ -2,74 +2,76 @@
 var generateBtn = document.querySelector("#generate");
 
 // arrays to select from
-const lowerCase = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
-const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const specialChar = ['!', '"', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "01234567890";
+const specialChar = "!#$%&'()*+,-./<=>?@[]^_`{}|~";
 
-
-function characterAmt() {
-  var characterPrompt = parseInt(window.prompt("How many characters in your password? (8-128)"));
-  // Conditional Recursive Function Call
-  if (characterPrompt < 8 || characterPrompt > 128 || characterPrompt === "" || characterPrompt === null) {
-    window.alert("You need to provide a valid answer! Please try again.")
-    // use return to call it again and stop the rest of this function from running
-    return characterAmt()
-    
-  };
-  return characterPrompt
-}
-
-// function 
-
-console.log(characterAmt())
 
 // Write password to the #password input
-function writePassword() {
+function genPassword() {
   window.alert("Let's generate a password!")
-  characterAmt()
-  var genPassword = []
-  var confirmLower = confirm("Do you want lowercase letters?")
-  var confirmUpper = confirm("Do you want uppercase letters?")
-  var confirmNum = confirm("Do you want numbers?")
-  var confirmSpecial = confirm("Do you want special characters?")
 
-    if (confirmLower === true) {
-      genPassword.push (lowerCase[Math.floor(Math.random()*lowerCase.length)])
+    
+    var characterPrompt = parseInt(prompt("How many characters in your password? (8-128)"));
+    if (characterPrompt < 8 || characterPrompt > 128 || characterPrompt === "" || characterPrompt === null) {
+      alert("You need to provide a valid answer! Please try again.")
+      return genPassword();
+    }
+
+console.log(characterPrompt)
+
+  let characterAmt = "";
+  var confirmLower = confirm("Do you want to include lowercase letters?")
+  var confirmUpper = confirm("Do you want to include uppercase letters?")
+  var confirmNum = confirm("Do you want to include numbers?")
+  var confirmSpecial = confirm("Do you want to include special characters?")
+
+    if (!confirmLower && !confirmUpper && !confirmNum && !confirmSpecial) {
+      alert("You have selected to have no password. Please select at least one option!")
+      return genPassword();
     }
     
-    if (confirmUpper === true) {
-      genPassword.push (upperCase[Math.floor(Math.random()*upperCase.length)])
-    }
+  while (characterAmt.length < characterPrompt) {
 
-    if (confirmNum === true) {
-      genPassword.push (numbers[Math.floor(Math.random()*numbers.length)])
-    }
+    if (confirmLower) {
+      characterAmt += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        if (characterAmt.length === characterPrompt) {
+          break;
+        }
+      }
 
-    if (confirmSpecial === true) {
-      genPassword.push (specialChar[Math.floor(Math.random()*specialChar.length)])
-    }
- return genPassword
-  }
+    if (confirmUpper) {
+      characterAmt += upperCase[Math.floor(Math.random() * upperCase.length)];
+        if (characterAmt.length === characterPrompt) {
+          break;
+        }
+      }
 
+    if (confirmNum) {
+      characterAmt += numbers[Math.floor(Math.random() * numbers.length)];
+        if (characterAmt.length === characterPrompt) {
+          break;
+        }
+      } 
 
+    if (confirmSpecial) {
+      characterAmt += specialChar[Math.floor(Math.random() * specialChar.length)];
+        if (characterAmt.length === characterPrompt) {
+          break;
+        }
+      }
+      console.log (genPassword)
+    };
+  return genPassword
+}
 
-
-// var password = generatePassword();
-// var passwordText = document.querySelector("#password");
-// passwordText.value = password;
-
+function writePassword() {
+  var password = genPassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
 
-
-// to-do list
-// math.random and math.round to select something at random from an array and round to pick a whole number
-// use Robot Gladiators to understand the logic in the window alert functions
-//
-
-
-// confirmLower();
-// confirmUpper(); 
-// confirmSpecial();
